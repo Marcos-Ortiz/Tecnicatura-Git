e10143865a554317df69972984f2212cddf744cc
Marcos Ortiz <marzgabriel@outlook.com>
2025-05-08 11:08:48 -0300

    Leccion1 de Python 08/05/25 11:09

---
+++ Python/Leccion1/main.py
@@ -0,0 +1,16 @@
+miVariable = 3
+print(miVariable)
+miVariable = "Hola a todos los alumnos de la tecnicatura"
+print(miVariable)
+miVariable = 3.5
+print(miVariable)
+x = 10
+y = 2
+z = x + y
+print(z)
+print(id(x))
+# Las literales se escriben x064(aqui esta guardada la literal x=10)
+print(id(y))
+print(id(z))
+# El resto de variables estan en x808(y=2) y x128(z=x+y)
+# En mi caso no han cambiado las direcciones de memoria

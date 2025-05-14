93ea5f884e83c50daeabcc896a4efaf714c5f37d
Marcos Ortiz <marzgabriel@outlook.com>
2025-05-14 10:16:35 -0300

    Creada la carpeta class-git junto con archivo README.txt. Además está generado el archivo HolaMundo2.java

---
+++ Java/Leccion1/HolaMundo2/src/HolaMundo.java
@@ -0,0 +1,16 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo desde Java");
+        
+        int miVariable = 10;
+        System.out.println(miVariable);
+        miVariable = 5;
+        System.out.println(miVariable);
+        //Tipo String
+        String miVaraibleCadena = "Bienvenidos";
+        System.out.println(miVaraibleCadena);
+        miVaraibleCadena = "Sigamos creciendo en programacion";
+        System.out.println(miVaraibleCadena);
+    }
+        
+}
